ee30de3753fa3078629994ffa19b36595432f58e
Ferdinando Prates <ferdinando.prates@intecomlogistica.com.br>
2022-04-21 11:40:42 -0300

    criado botões de digitação

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,12 @@
 <template>
   <q-page class="flex column">
     <div class="col q-pa-lg">
-      <q-input v-model="busca" placeholder="Busca" dark borderless>
+      <q-input v-model="busca" placeholder="Digite o Local" dark borderless @keyup.enter="pegarlocalizacaoBusca()">
         <template v-slot:before>
-          <q-icon @click="pegarlocalizacao" name="my_location" class="cursor-pointer" />
+          <q-icon @click="pegarlocalizacaoBusca" name="my_location" class="cursor-pointer" />
         </template>
         <template v-slot:append>
-          <q-icon name="search" class="cursor-pointer" />
+          <q-btn dense flat round icon="search" @click="pegarlocalizacaoBusca" />
         </template>
       </q-input>
     </div>
@@ -31,8 +31,7 @@
     <template v-else>
       <div class="col column text-center text-white">
         <div class="col text-h2 text-weight-thin">
-          Quasar
-          <br />Weather
+          Clima
         </div>
 
         <q-btn class="col" flat @click="pegarlocalizacao">
@@ -43,77 +42,111 @@
     </template>
     <div class="col skyline"></div>
     <div class="footer text-grey text-subtitle2">
-       FERDINANDO PRATES ©2022
+      FERDINANDO PRATES ©2022
     </div>
   </q-page>
 </template>
 
 <script>
-export default {
+  export default {
 
-  name: 'PageIndex',
+    name: 'PageIndex',
 
-  data() {
-    return {
-      busca: '',
-      weatherData: null,
-      lat: null,
-      lon: null,
-      apiurl: 'https://api.openweathermap.org/data/2.5/weather?',
-      apikey: 'fba5e11aa56a96d0cdb1fd118c881959'
-    }
-  },
+    data() {
+      return {
+        busca: '',
+        weatherData: null,
+        lat: null,
+        lon: null,
+        apiurl: 'https://api.openweathermap.org/data/2.5/weather?',
+        apikey: 'fba5e11aa56a96d0cdb1fd118c881959'
+      }
+    },
 
-  methods: {
-    pegarlocalizacao() {
-      //console.log('pegarlocalizacao');
+    methods: {
 
-      navigator.geolocation.getCurrentPosition
-        (position => {
-          //console.log('positio: ', position)
-          this.lat = position.coords.latitude
-          this.lon = position.coords.longitude
-          this.getWeatherByCoords()
+      pegarlocalizacaoBusca() {
+
+        if (!this.busca) {
+
+          this.$q.notify({
+            color: "orange-8",
+            textColor: "black",
+            icon: "warning",
+            message: "Digite um Local",
+          })
+
+        } else {
+          this.weatherData = ''
+          this.$axios(`${this.apiurl}q=${this.busca}&appid=${this.apikey}&lang=pt_br&units=metric`)
+            .then(resp => {
+              this.lat = resp.data.coord.lat
+              this.lon = resp.data.coord.lon
+              this.getWeatherByCoords()
+              this.busca = ''
+            })
+
+            .catch(err => {
+              this.busca = ''
+              this.$q.notify({
+                color: "orange-8",
+                textColor: "black",
+                icon: "warning",
+                message: "Erro - Verifique o Local Digitado",
+              })
+            })
         }
-        )
-    },
+      },
 
-    getWeatherByCoords() {
+      pegarlocalizacao() {
 
-      this.$axios(`${this.apiurl}lat=${this.lat}&lon=${this.lon}&appid=${this.apikey}&lang=pt_br&units=metric`)
-        .then(resp => {
-          console.log(resp);
-          this.weatherData = resp.data
-        })
-    },
+        navigator.geolocation.getCurrentPosition
+          (position => {
+
+            this.lat = position.coords.latitude
+            this.lon = position.coords.longitude
+            this.getWeatherByCoords()
+          }
+          )
+      },
+
+      getWeatherByCoords() {
+
+        this.$axios(`${this.apiurl}lat=${this.lat}&lon=${this.lon}&appid=${this.apikey}&lang=pt_br&units=metric`)
+          .then(resp => {
+
+            this.weatherData = resp.data
+          })
+      },
+
+    }
 
   }
-
-}
 </script>
+
 
 
 <style lang="sass">
 
-.q-page
-  background: linear-gradient(to bottom, #136a8a, #267871)
-.degree
-  top: -40px
-.skyline
-  flex: 0 0 110px
-  background: url(../assets/skyline.png)
-  background-size: contain
-  background-position: center bottom
-.footer
-  position: absolute
-  bottom: 0
-  width: 100vw
-  font-family: sans-serif
-  font-size: 10px
-  color: white
-  display: flex
-  align-items: center
-  justify-content: center
-  background: black
-  opacity: 0.8
-</style>
+  .q-page
+    background: linear-gradient(to bottom, #304352, #d7d2cc)
+  .degree
+    top: -40px
+  .skyline
+    flex: 0 0 110px
+    background: url(../assets/skyline.png)
+    background-size: contain
+    background-position: center bottom
+  .footer
+    position: absolute
+    bottom: 0
+    width: 100vw
+    font-family: sans-serif
+    font-size: 10px
+    color: white
+    display: flex
+    align-items: center
+    justify-content: center
+    background: black
+    opacity: 0.8
+  </style>
